e7159c3a54212309aac37150558e441976e9bd28
Ebenezer F <s3404137@leidenuniv.nl>
2023-11-30 16:59:21 +0100

    Toevoegingen

--- assignment2/opdr2.java
+++ assignment2/opdr2.java
@@ -48,24 +48,32 @@ class SyntaxTree
     public int index;
 
     public SyntaxTree (String newExpr){
-        this.root = parseExpression(expr);
         this.expr = newExpr;
         this.index = 0;
+        this.root = parseExpression(newExpr);
 
+        while (expr.length() > 0){
+            this.root = parseExpression(expr);
+        }
         //System.out.println(newExpr);
     }
 
     public Node parseVariable (){
         StringBuilder var = new StringBuilder();
+       
 
-        while(Character.isLetterOrDigit(expr.charAt(index)) ){
+        while(index < expr.length() && Character.isLetterOrDigit(expr.charAt(index))){
             var.append(expr.charAt(index));
             index++;
         }
-
-        expr = expr.substring(index);
+        
+        if (index < expr.length()){       // CHECK ABC DEF GHI
+            expr = expr.substring(index); //
+           
+            
+        }
+        
         index = 0;
-
         Node varNode = new Node(var.toString());
         
         return varNode;
@@ -74,30 +82,84 @@ class SyntaxTree
     public Node parseLambda (){
         Node lambdaNode = new Node("\\");
 
-        Node varNode =  parseVariable(); // parameter meegeven
+        Node varNode =  parseVariable();
         if (varNode != null){
             lambdaNode.setLeft(varNode);
         }
+        
+        while (expr.charAt(index) == ' '){
+            index++;
+        }
 
-        Node exprNode = parseExpression(expr); // parameter meegeven
+        expr = expr.substring(index);
+        index = 0;
+
+        Node exprNode = parseExpression(expr);
         if (exprNode != null){
             lambdaNode.setRight(exprNode);
         }
 
         return lambdaNode;
     }
-
-    public Node parseExpression (String expr){
+    
+    public Node parseExpression (String newExpr){
         Node newRoot = new Node("");
-        //Character huidigeKar = expr.charAt(index);
+        Character huidigeKar = expr.charAt(index);
 
-        // if (huidigeKar == '\\'){
-        //     //index++;
-        //     //expr = expr.substring(index);
-        //     newRoot = parseLambda();
+        //System.out.println(huidigeKar + " - " + expr + " - " + index);
+
+        if (huidigeKar == '\\'){
+            index++;
+            expr = newExpr.substring(index); // verwerk \
+
+            index = 0;
+            newRoot = parseLambda();
             
-        //     return newRoot;
-        // }
+            return newRoot;
+        } else if (Character.isLetter(huidigeKar)) {
+            newRoot = parseVariable();
+
+            return newRoot;
+        } else if (huidigeKar == '(') {
+            index++;
+            expr = newExpr.substring(index);
+
+            index = 0;
+            newRoot = parseExpression(expr);
+
+            index++;
+            expr = expr.substring(index);
+
+            return newRoot;
+        } else if (huidigeKar == ' '){
+            Node connectorNode = new Node("+");
+            Node leftNode = root;
+
+            index++;
+            expr = newExpr.substring(index);      
+
+            
+            Node rightNode = parseExpression(expr);
+            
+            if (leftNode != null){
+                connectorNode.setLeft(leftNode);
+            }
+
+            if (rightNode != null){
+                connectorNode.setRight(rightNode);
+            }
+
+            root = connectorNode;
+
+            System.out.println(expr + " " + index);
+
+            return root;
+        }
+
+
+        
+
+        //expr == var || (expr) || expr expr || \var expr
 
         // if(huidigeKar == '\\'){
         //     if (volgendeKar != )
@@ -110,13 +172,31 @@ class SyntaxTree
             
         // }
 
+        // abc \x abc def
+            //      +
+            // abc      +
+            //      ()     def
+            
+            //      +
+            // abc    
 
+            // ________________
+            //|                | 
+            //|    ebe         | 
+            //|                | 
+            //|________________| <-- keuken
+
+            // wat s het probleeem ook alweer
+            // abc def 
+
+            // 
 
 
         return newRoot;
     }
 
     public String getRootContent(){
+        // System.out.println("Root content printed");
         return root.getContent();
     }
 }
@@ -128,11 +208,14 @@ class Main
     public static void main(String args[])
     {
         // inlezen
-
-        String input = "\\x abc";
+        String input = "abc def ghi";
 
         SyntaxTree boom = new SyntaxTree(input);
-        System.out.println(boom.getRootContent());
+        System.out.println(boom.root.getContent());
+        System.out.println(boom.root.getLeft().getContent());
+        System.out.println(boom.root.getLeft().getLeft().getContent());
+        System.out.println(boom.root.getLeft().getRight().getContent());
+        System.out.println(boom.root.getRight().getContent());
 
 
         // String fileName = "input.txt";
